e630d0f2f08eb1f6c46d9444be4fa08c8098ec8d
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2022-06-10 23:55:37 +0300

    second sec

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,9 @@ export default function Home() {
           <h1 className={styles.first__text}><span ref={firsTitleBox}  className={styles.animation__span}  >СМОТРЕТЬ</span></h1>
             <div className={styles.white__arrow} >
              <span ref={whiteArrow} className={styles.animation__span} >
-              <Image src={WhiteArrow.src}  width={70} height={15} />
+          
+             <svg viewBox="0 0 44 11" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="#fff" fill-rule="evenodd" clip-rule="evenodd" d="M0 5.5H43V6.5H0V5.5Z"></path><path fill="#fff" d="M43.9447 5.90506L35.167 0.914272C35.051 0.848274 34.9381 1.00447 35.0394 1.09088L40.6984 5.91727C40.75 5.96124 40.7492 6.04024 40.6968 6.08323L35.0655 10.7047C34.9624 10.7893 35.0727 10.9477 35.19 10.8835L43.9431 6.09454C44.0182 6.05345 44.0191 5.94737 43.9447 5.90506Z"></path></svg>
+              {/* <Image src={WhiteArrow.src}  width={70} height={15} /> */}
              </span> 
             </div>
 
